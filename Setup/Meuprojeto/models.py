3c0c17a076a96f120b00325c83a2c76375c56972
marcelohiginoduarte <marcelihigino@gmail.com>
2024-07-08 17:29:45 -0300

    Criaçção do projeto de Gestao

---
+++ Setup/Meuprojeto/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Servico(models.Model):
+    Status_projeto = {
+        "Em Programacao": "Em programacao",
+        "Programado": "Programado",
+        "Em execucao": "Em execucao",
+        "Executado": "Executado",
+        "Executado Fora Prazo": "Executado Fora Prazo",
+        "Nao Executado": "Nao Executado",
+        "Interropida": "Interropida",
+        "Medicao": "Medicao",
+        "Entregue": "Entregue",
+        "Pago": "Pago",
+    }
+
+    Projeto = models.CharField(max_length=12)
+    Nota = models.CharField(max_length=10)
+    Status = models.CharField(max_length= 20)
+    Descricao = models.CharField(max_length=50)
+    Local = models.CharField(max_length=50)
+    Data_Inicio = models.DateField(blank=True, null=True)
+    Data_Fim = models.DateField(blank=True, null=True)
+    Data_Programacao = models.DateField(blank=True, null=True)
+    Equipe = models.CharField(max_length=5)
+    valor_inicial = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_final = models.DecimalField(max_digits=10, decimal_places=2)
+    
+
+    def __str__(self) -> str:
+        return self.Projeto
+
+
+
+class Equipe(models.Model):
+    NumeroEquipe = models.CharField(max_length=5)
+    Nome_Encaregado = models.CharField(max_length=100)
+    Matricula_Encaregado = models.CharField(max_length=100)
+    Nome_Eletricista_1 = models.CharField(max_length=100)
+    Matricula_Eletricista1 = models.CharField(max_length=8)
+    Nome_Eletricista_2 = models.CharField(max_length=100)
+    Matricula_Eletricista2 = models.CharField(max_length=8)
+    Nome_Eletricista_3 = models.CharField(max_length=100)
+    Matricula_Eletricista3 = models.CharField(max_length=8)
+    Nome_Eletricista_4 = models.CharField(max_length=100)
+    Matricula_Eletricista4 = models.CharField(max_length=8)
+    Nome_Ajudante_1 = models.CharField(max_length=100)
+    Matricula_Ajudante1 = models.CharField(max_length=8)
+
+    def __str__(self) -> str:
+        return self.NumeroEquipe
---
+++ Setup/Meuprojeto/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+from Meuprojeto.models import Servico, Equipe
+
+def home(request):
+    return render(request, 'Home.html',)
+
+def CadastrarServico(request):
+    if request.method == "GET":
+        return render (request, 'Cadastrarservico.html')
+    if request.method == "POST":
+        Projeto = request.POST.get('Projeto')
+        Nota = request.POST.get('nota')
+        Status = request.POST.get('status')
+        Descricao = request.POST.get('Descricao')
+        Local = request.POST.get('Local')
+        Data_Inicio = request.POST.get('Data_Inicio')
+        Data_Fim = request.POST.get('Data_Fim')
+        Data_Programacao = request.POST.get('Data_Programacao')
+        Equipe = request.POST.get('Equipe')
+        valor_inicial = request.POST.get('valor_inicial')
+        valor_final = request.POST.get('valor_final')
+
+        servico = Servico(
+            Projeto = Projeto,
+            Nota = Nota,
+            Status = Status,
+            Descricao = Descricao,
+            Local = Local,
+            Data_Inicio = Data_Inicio,
+            Data_Fim = Data_Fim,
+            Data_Programacao = Data_Programacao,
+            Equipe = Equipe,
+            valor_inicial = valor_inicial,
+            valor_final = valor_final
+        )
+        #Salvar os dados no banco de dados
+        servico.save()
+    return render(request, 'Cadastrarservico.html',)
+
+def CadastroEquipes(request):
+    if request.method == "GET":
+        return render(request, 'Cadastroequipes.html')
+    elif request.method == "POST":
+        NumeroEquipe = request.POST.get('NumeroEquipe')
+        Nome_Encaregado = request.POST.get('Nome_Encaregado')
+        Matricula_Encaregado = request.POST.get('Matricula_Encaregado')
+        Nome_Eletricista_1 = request.POST.get('Nome_Eletricista_1')
+        Matricula_Eletricista1 = request.POST.get('Matricula_Eletricista1')
+        Nome_Eletricista_2 = request.POST.get('Nome_Eletricista_2')
+        Matricula_Eletricista2 = request.POST.get('Matricula_Eletricista2')
+        Nome_Eletricista_3 = request.POST.get('Nome_Eletricista_3')
+        Matricula_Eletricista3 = request.POST.get('Matricula_Eletricista3')
+        Nome_Eletricista_4 = request.POST.get('Nome_Eletricista_4')
+        Matricula_Eletricista4 = request.POST.get('Matricula_Eletricista4')
+        Nome_Ajudante_1 = request.POST.get('Nome_Ajudante_1')
+        Matricula_Ajudante1 = request.POST.get('Matricula_Ajudante1')
+
+
+        equipe = Equipe(
+           NumeroEquipe = NumeroEquipe,
+           Nome_Encaregado = Nome_Encaregado,
+           Matricula_Encaregado = Matricula_Encaregado,
+           Nome_Eletricista_1 = Nome_Eletricista_1,
+           Matricula_Eletricista1 = Matricula_Eletricista1,
+           Nome_Eletricista_2 = Nome_Eletricista_2,
+           Matricula_Eletricista2 = Matricula_Eletricista2,
+           Nome_Eletricista_3 = Nome_Eletricista_3,
+           Matricula_Eletricista3 = Matricula_Eletricista3,
+           Nome_Eletricista_4 = Nome_Eletricista_4, 
+           Matricula_Eletricista4 = Matricula_Eletricista4,
+           Nome_Ajudante_1 = Nome_Ajudante_1,
+           Matricula_Ajudante1 = Matricula_Ajudante1
+        )
+
+        equipe.save()
+        return render(request, 'Cadastroequipes.html')
