fc40b5d941bdf5ea20382a82faaa06227e109cb4
marcelohiginoduarte <marcelihigino@gmail.com>
2024-07-08 18:49:05 -0300

    Add admin

--- Setup/Meuprojeto/admin.py
+++ Setup/Meuprojeto/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from Meuprojeto.models import Servico, Equipe
 
-# Register your models here.
+
+
+
+class ListandoServico(admin.ModelAdmin):
+    list_display = ("Projeto", "Nota", "Status")
+    list_display_links = ("Projeto", "Nota")
+    
+
+class ListandoEquipe(admin.ModelAdmin):
+    list_display = ("NumeroEquipe", "Nome_Encaregado", "Matricula_Encaregado")
+    list_display_links = ("NumeroEquipe", "Nome_Encaregado", "Matricula_Encaregado")
+
+admin.site.register(Servico,ListandoServico)
+admin.site.register(Equipe, ListandoEquipe)
