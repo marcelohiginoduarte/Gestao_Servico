67905c086db696aa5e64250a070333f973772bfb
marcelohiginoduarte <marcelihigino@gmail.com>
2024-07-09 11:02:36 -0300

    Visualização dos dados

--- Setup/Meuprojeto/views.py
+++ Setup/Meuprojeto/views.py
@@ -84,7 +84,7 @@ def CadastroEquipes(request):
 def VisualizarServicos(request):
     visualizar_servicos = Servico.objects.all()
     print(visualizar_servicos)
-    return render(request, 'Visualizar.html',)
+    return render(request, 'Visualizar.html', {'visualizer_servicos': visualizar_servicos})
 
 def StatusServico(request):
     if Status == '':
